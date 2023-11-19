370fb5e0db1ae20b67a768d5cdfad730b2ed522b
Nikita Ischenko <107185196+DragonAirDragon@users.noreply.github.com>
2023-11-20 00:09:53 +0300

    fsdsfgfdg

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Ищенко Никита Николаевич 4";
+  const greeting = "Ищенко Никита Николаевич 5";
   return (
     <div className="App">
      <header className="App-header">
