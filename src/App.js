636e5e00ffb0541aa92c95a8285a704cc8a589f3
Nikita Ischenko <107185196+DragonAirDragon@users.noreply.github.com>
2023-11-19 22:49:37 +0300

    Defaulte 1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 
 function App() {
   const greeting = "Ищенко Никита Николаевич";
-
-  console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
