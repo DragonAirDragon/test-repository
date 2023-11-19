a7d7174672088b31eba4f3cb25fd64a855bbcf29
Nikita Ischenko <107185196+DragonAirDragon@users.noreply.github.com>
2023-11-19 23:39:32 +0300

    update 2342

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Ищенко Никита Николаевич 3";
+  const greeting = "Ищенко Никита Николаевич 4";
   return (
     <div className="App">
      <header className="App-header">
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders greeting message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Ищенко Никита Николаевич 3/i);
+  const linkElement = screen.getByText(/Ищенко Никита Николаевич 4/i);
   expect(linkElement).toBeInTheDocument();
 });
