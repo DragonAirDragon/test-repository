82aa16ef27fa4db36f891751c43a1c278dc96b7f
Nikita Ischenko <107185196+DragonAirDragon@users.noreply.github.com>
2023-11-20 05:20:34 +0300

    feat
     feat: Added semantic-release

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Ищенко Никита Николаевич 9";
+  const greeting = "Ищенко Никита Николаевич release";
   return (
     <div className="App">
      <header className="App-header">
