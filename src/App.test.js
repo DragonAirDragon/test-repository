dfc8097b953b529278d4876850f7de10009f6684
Nikita Ischenko <107185196+DragonAirDragon@users.noreply.github.com>
2023-11-20 06:59:43 +0300

    chore!: drop support for Node 6
    BREAKING CHANGE: use JavaScript features not available in Node 6.

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders greeting message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Ищенко Никита Николаевич release 1/i);
+  const linkElement = screen.getByText(/Ищенко Никита Николаевич release without node 6/i);
   expect(linkElement).toBeInTheDocument();
 });
